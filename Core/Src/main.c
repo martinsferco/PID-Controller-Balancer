6c9521d1e9144ea931c4c9ad4884ff163655cdf1
Martín Sferco <martinsferco@hotmail.com>
2026-08-19 22:03:29 -0300

    Configurar el ADC del potenciometro y el timer de 100 ms

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -19,6 +19,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "cmsis_os.h"
+#include "adc.h"
 #include "tim.h"
 #include "usart.h"
 #include "gpio.h"
@@ -93,6 +94,8 @@ int main(void)
   MX_TIM2_Init();
   MX_USART2_UART_Init();
   MX_TIM3_Init();
+  MX_ADC1_Init();
+  MX_TIM4_Init();
   /* USER CODE BEGIN 2 */
   App_Init();   /* crea semaforo + tasks (definido en App/Src/app.c) */
   /* USER CODE END 2 */
